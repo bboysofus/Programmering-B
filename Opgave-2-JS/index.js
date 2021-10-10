9783da5bb94e72bb048f6328fca2645e3255ef71
bboysofus <sofusba@gmail.com>
2021-10-10 17:13:12 +0200

    Names

--- Opgave-2-JS/index.js
+++
@@ -1,8 +0,0 @@
-let main = document.querySelector('main')
-let button = document.querySelector('#button')
-
-const skiftBillede = () => {
-    main.style.backgroundImage = 'url("./assets/1.jpg")'
-}
-
-button.addEventListener('click', skiftBillede)
