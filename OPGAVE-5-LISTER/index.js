f01bb2d66df850059a132ecdf29f28e3e39cc7b6
bboysofus <sofusba@gmail.com>
2021-10-10 19:01:45 +0200

    Searchbar added

--- OPGAVE-5-LISTER/index.js
+++ OPGAVE-5-LISTER/index.js
@@ -1,27 +1,41 @@
 let body = document.querySelector('body')
-let musik = ['EARFQUAKE', 'Hey You', 'Introvert', 'Jail', 'Jagt', 'Wet Dreamz', 'CORSO']
+let main = document.querySelector('main')
+let musikListe = ['EARFQUAKE', 'Hey You', 'Introvert', 'Jail', 'Jagt', 'Wet Dreamz', 'CORSO']
+let search = document.getElementById('search')
 
-musik.map(sang => {
-    let card = document.createElement('div')
-    card.classList.add('card')
+musikListe.push('Latch')
 
-    let top = document.createElement('top')
-    top.classList.add('top')
+const addCard = nummer => {
+    
+        let newCard = document.createElement('div')
+        newCard.classList.add('card')
 
-    let bottom = document.createElement('bottom')
-    bottom.classList.add('bottom')
+        let top = document.createElement('div')
+        top.classList.add('top')
 
-    let h = document.createElement('h1')
-    h.innerHTML = sang
+        let bottom = document.createElement('div')
+        bottom.classList.add('bottom')
 
-    let p = document.createElement('p')
-    p.innerHTML = 'Find den på Spotify!'
-    p.style.fontSize = '1.5rem'
+        let h = document.createElement('h1')
+        h.innerHTML = nummer
 
-    card.append(top)
-    card.append(bottom)
-    top.append(h)
-    bottom.append(p)
+        let p = document.createElement('p')
+        p.innerHTML = 'Find den på Spotify!'
+        p.style.fontSize = '1.5rem'
 
-    body.append(card)
+        newCard.append(top)
+        newCard.append(bottom)
+        top.append(h)
+        bottom.append(p)
+
+        main.append(newCard)
+    }
+
+
+musikListe.map( nummer => addCard(nummer) )
+
+search.addEventListener('input', () => {
+    let results = musikListe.filter(nummer => nummer.toLowerCase().includes(search.value.toLowerCase()))
+    main.innerHTML = ''
+    results.map(nummer => addCard(nummer))
 })
