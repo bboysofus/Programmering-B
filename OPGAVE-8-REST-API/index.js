8d0da08453db0359b26f23873c657c8f52b9ccfd
bboysofus <sofusba@gmail.com>
2021-11-15 23:59:22 +0100

    Færdig opgave

--- OPGAVE-8-REST-API/index.js
+++ OPGAVE-8-REST-API/index.js
@@ -5,34 +5,56 @@ let price = document.querySelector('#price')
 let cat = document.querySelector('#cat')
 
 let participantSelect = document.querySelector('#participant-select')
+let activitySelect = document.querySelector('#activity-select')
 
 const getActivity = () => {
-    title.innerHTML = ''
+    if(participantSelect.value == '' || activitySelect.value == ''){
+        alert("Please select an activity and the number of participants!")
+        location.reload()
+    }
+    title.innerHTML = 'Loading...'
     participants.innerHTML = ''
     acc.innerHTML = ''
     price.innerHTML = ''
     cat.innerHTML = ''
 
     fetch('https://www.boredapi.com/api/activity'
-    +participantSelect.value
+    + '?participants=' + participantSelect.value
+    + '&type=' + activitySelect.value
     )
-
-
         .then( response => response.json() )
             .then( json => {
                 console.log(json)
                 createCard(json)
-            } )    
+                if(title.innerHTML == 'undefined' && activitySelect.value == 'diy'){
+                    alert("There's no activities with this amount of participants!")
+                    location.reload()
+                }
+                if(title.innerHTML == 'undefined' && activitySelect.value == 'music'){
+                    alert("There's no activities with this amount of participants!")
+                    location.reload()
+                }
+                if(title.innerHTML == 'undefined' && activitySelect.value == 'social'){
+                    alert("There's no activities with this amount of participants!")
+                    location.reload()
+                }
+                if(title.innerHTML == 'undefined' && activitySelect.value == 'cooking'){
+                    alert("There's no activities with this amount of participants!")
+                    location.reload()
+                }
+                if(title.innerHTML == 'undefined' && activitySelect.value == 'education'){
+                    alert("There's no activities with this amount of participants!")
+                    location.reload()
+                }
+            } )
 }
 
 const createCard = a => {
     document.querySelector('#title').innerHTML = a.activity
     document.querySelector('#participants').innerHTML = 'Participants: ' + a.participants
-    document.querySelector('#acc').innerHTML = 'Type: ' + a.type
+    document.querySelector('#acc').innerHTML = 'Activity type: ' + a.type
     document.querySelector('#price').innerHTML = 'Price: ' + a.price
     document.querySelector('#cat').innerHTML = 'Key: ' + a.key
 }
-
-getActivity()
 
 document.querySelector('#fetch').addEventListener('click', getActivity)
