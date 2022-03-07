a7bb7e9d20cece4054f800bba43b17f3bbb3d929
bboysofus <sofusba@gmail.com>
2022-03-07 15:41:41 +0100

    månebase img changes

--- moonbase/index.js
+++ moonbase/index.js
@@ -2,8 +2,11 @@ img2 = document.querySelector(".page2-img-container")
 
 const myScrollFunc1 = function() {
     var y = window.scrollY;
-    if (y >= 900) {
+    if (y >= 900 && y <= 1500) {
         img2.style.opacity = "100%"
+    }
+    else if (y > 1500) {
+        img2.style.opacity = "0%"
     }
     else{
         img2.style.opacity = "0%"
