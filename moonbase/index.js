ef1c45dfc1e7918fa800ccdd8bec2af5b35b2151
bboysofus <sofusba@gmail.com>
2022-03-07 16:20:16 +0100

    updates

--- moonbase/index.js
+++ moonbase/index.js
@@ -1,15 +1,19 @@
 img2 = document.querySelector(".page2-img-container")
+text1 = document.querySelector(".page2-text-container")
 
 const myScrollFunc1 = function() {
     var y = window.scrollY;
-    if (y >= 900 && y <= 1500) {
-        img2.style.opacity = "100%"
+    if (y >= 700 && y <= 1500) {
+        img2.style.opacity = "100%";
+        text1.style.opacity = "100%";
     }
     else if (y > 1500) {
-        img2.style.opacity = "0%"
+        img2.style.opacity = "0%";
+        text1.style.opacity = "0%"
     }
     else{
         img2.style.opacity = "0%"
+        text1.style.opacity = "0%"
     }
 }
 
