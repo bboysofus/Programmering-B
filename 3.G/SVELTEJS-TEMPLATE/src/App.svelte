4ecbf9383a11457f55616ed865e6eca0e18b9961
brofus1 <sofusba@gmail.com>
2023-02-01 11:12:28 +0100

    Svelte Template

---
+++ 3.G/SVELTEJS-TEMPLATE/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+
+</script>
+
+<main>
+	<h1>Hello svelte!</h1>
+</main>
+
+<style>
+	:global(*, body){
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;	
+	}
+
+	main{
+		display: grid;
+		place-items: center;
+	}
+
+	h1{
+		text-transform: uppercase;
+		font-size: 2rem;
+		font-weight: 100;
+	}
+
+</style>
